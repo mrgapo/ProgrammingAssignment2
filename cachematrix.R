5d5ab05df69e668cdf821a8e2f2f67a72700f03c
Gasper Mis <mrgapo@gmail.com>
2015-03-22 18:50:12 +0100

    -- done phase 1

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,39 @@
 ## Write a short comment describing this function
 
 
+## macke cache matrix is a contrust where we set matrix and later retrive data if needed agai
 makeCacheMatrix <- function(x = matrix()) {
-		## should do someting here
+  i <- NULL
+ 
+  set <- function(x) {
+    m <<- x
+    i <<- NULL
+  }
+  
+  get <- function(m)
+  
+  setinverse <- function(inverse) i <<- inverse
+  
+  getinverse <- function() i
+  
+  list(set = set, get = get,
+       ##setinverse = setinverse,
+       getinverse = getinverse)
+    
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  
+  data <- x$get()
+  i <- solve(data)
+  x$setmean(i)
+  i
 }
